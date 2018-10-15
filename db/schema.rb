7efda7c03dd12585f02c5243e33846c759dfdd23
Katheryn Osmond <katheryn.osmond@excella.com>
2018-10-15 21:00:31 +0000

    cleaned up code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_025352) do
+ActiveRecord::Schema.define(version: 2018_10_15_035240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_10_15_025352) do
     t.decimal "total", precision: 12, scale: 3
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "sales_tax"
   end
 
   create_table "products", force: :cascade do |t|
